cb4f25eb99368e94d1c8e144a46f298c5b7e49c7
tairoroberto <tairoroberto@hotmail.com>
2018-06-15 16:09:54 -0300

    Ajuste de cadastro de usuario

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,85 @@ const router = express.Router();
 
 admin.initializeApp(functions.config().firebase);
 
-/*############### USER ###############*/
+/*############### User ###############*/
+app.route("/user")
+    .get((req, res) => {
+        const userRef = admin.database().ref('/users');
+        userRef
+            .once('value')
+            .then((snapshot) => {
+
+                let events = [];
+                snapshot.forEach((data) => {
+                    let event = data.val();
+                    event.uid = data.key;
+                    events.push(event)
+                });
+                return res.status(200).send({"success": true, "users": events});
+            })
+            .catch((error) => {
+                console.log("Erro ao buscar dados:", error);
+                return res.status(500).send({"success": false, "message": "Erro ao buscar dados: " + error});
+            });
+    })
+    .post((req, res) => {
+        const userRef = admin.database().ref('/users');
+
+        userRef.push({
+            name: req.body.name,
+            description: req.body.description,
+            date: req.body.date,
+            image: req.body.image,
+            local: req.body.local,
+            sponsor: req.body.sponsor,
+            created_at: new Date(Date.now()).toISOString()
+        }).then(() => {
+            return res.status(200).send({"success": true, "message": "Usuário salvo com sucesso"});
+        }).catch((error) => {
+            console.log("Erro ao salvar usuário:", error);
+            return res.status(500).send({"success": false, "message": "Erro ao salvar usuário: " + error});
+        });
+
+    }).put((req, res) => {
+    const userRef = admin.database().ref('/users/' + req.body.uid);
+    userRef
+        .once('value')
+        .then((snapshot) => {
+            let event = {};
+            event.name = req.body.name;
+            event.description = req.body.description;
+            event.date = req.body.date;
+            event.image = req.body.image;
+            event.local = req.body.local;
+            event.sponsor = req.body.sponsor;
+
+            return userRef.set(event)
+                .then(() => {
+                    return res.status(200).send({"success": true, "message": "Usuário atualizado com sucesso"});
+                })
+                .catch((error) => {
+                    console.log("Erro ao salvar usuário:", error);
+                    return res.status(500).send({"success": false, "message": "Erro ao atualizar usuário: " + error});
+                });
+        })
+        .catch((error) => {
+            console.log("Erro ao atualizar dados do usuário:", error);
+            return res.status(500).send({"success": false, "message": "Erro ao atualizar dados do usuário: " + error});
+        });
+}).delete((req, res) => {
+    const userRef = admin.database().ref('/users/' + req.body.uid);
+    userRef
+        .remove()
+        .then(() => {
+            return res.status(200).send({"success": true, "message": "Usuário deletado com sucesso"});
+        })
+        .catch((error) => {
+            console.log("Erro ao deletar dados do usuário:", error);
+            return res.status(500).send({"success": false, "message": "Erro ao atualizar dados do usuário: " + error});
+        });
+});
+
+/*############### Event ###############*/
 app.route("/event")
     .get((req, res) => {
         const eventRef = admin.database().ref('/events');
@@ -71,6 +149,17 @@ app.route("/event")
         })
         .catch((error) => {
             console.log("Erro ao atualizar dados do usuário:", error);
+            return res.status(500).send({"success": false, "message": "Erro ao atualizar dados do usuário: " + error});
+        });
+}).delete((req, res) => {
+    const eventRef = admin.database().ref('/events/' + req.body.uid);
+    eventRef
+        .remove()
+        .then(() => {
+            return res.status(200).send({"success": true, "message": "Evento deletado com sucesso"});
+        })
+        .catch((error) => {
+            console.log("Erro ao deletar dados do usuário:", error);
             return res.status(500).send({"success": false, "message": "Erro ao atualizar dados do usuário: " + error});
         });
 });
